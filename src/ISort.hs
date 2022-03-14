6adfa8dac28231b7a28cf9874231fb97d00b1528
Niki Vazou <nikivazou@gmail.com>
2022-03-14 16:35:03 +0100

    add cabal file

--- src/ISort.hs
+++ src/ISort.hs
@@ -5,6 +5,9 @@ import Prelude hiding (pure,(<*>))
 import RTick 
 
 insert :: (Ord a) => a -> [a] -> RTick [a]
+{-@ insert :: (Ord a) 
+           => x:a -> xs:[a] 
+           -> {t:RTick {os:[a] | len os == len xs + 1} | tcost t <= len xs } @-}
 insert x []   = pure [x]
 insert x (y:ys) 
   | x <= y    = pure (x:y:ys)
