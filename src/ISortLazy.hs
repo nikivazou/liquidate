20bbf1386ec2644cc589e1be68176431adab0006
Niki Vazou <nikivazou@gmail.com>
2022-03-15 08:10:58 +0100

    proved

--- src/ISortLazy.hs
+++ src/ISortLazy.hs
@@ -1,2 +1,49 @@
+{-@ LIQUID "--reflection"     @-}
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--no-totality"    @-}
+
 module ISortLazy where 
-    
+import Prelude hiding (pure,(<*>),(>>=), length)
+import List 
+import RTick 
+
+data LList a = LNil | LCons a (RTick (LList a))
+
+{-@ data LList a <p :: a -> a -> Bool> = 
+      LNil | 
+      LCons {lhd :: a, ltl :: RTick (LList <p> (a<p lhd>))} @-}    
+
+{-@ type SLList a = LList <{\x y -> x <= y}> a @-}
+
+{-@ insert :: Ord a => a -> xs:SLList a 
+           -> {t:RTick (SLList a) | tcost t <= 1 } @-}
+insert :: Ord a => a -> LList a -> RTick (LList a) 
+insert x LNil = pure (LCons x (pure LNil))
+insert x (LCons y ys)
+  | x <= y    = step (LCons x (pure (LCons y ys))) 
+  | otherwise = step (LCons y (ys >>= insert x))
+
+
+{-@ isort :: Ord a => xs:[a] 
+         -> {t:RTick (SLList a) | tcost t <= length xs} @-}
+isort :: Ord a => [a] -> RTick (LList a)
+isort []     = pure LNil
+isort (x:xs) = bbind 1 (isort xs) (insert x) 
+
+
+{-@ minimum :: Ord a => {v:[a] | 0 < length v} 
+            -> RTick a @-} 
+minimum :: Ord a => [a] -> RTick a 
+minimum xs = pure lhead <*> isort xs 
+
+
+
+
+{-@ measure llength @-}
+{-@ llength :: LList a -> Nat @-} 
+llength :: LList a -> Int 
+llength LNil = 0 
+llength (LCons _ (RTick _ xs)) = 1 + llength xs
+
+lhead :: LList a -> a 
+lhead (LCons x _) = x 
--- src/ISortSorted.hs
+++ src/ISortSorted.hs
@@ -1,1 +1,41 @@
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
+
 module ISortSorted where 
+
+import Prelude hiding (pure,(<*>),(>>=), length)
+import List 
+import Language.Haskell.Liquid.ProofCombinators 
+
+import RTick 
+import ISort 
+
+theorem :: Ord a => [a] -> () 
+{-@ theorem :: Ord a => xs:OList a 
+            -> { tcost (isort xs) <= length xs } @-} 
+theorem []  = () 
+theorem [_] = ()
+theorem (x:xs) 
+  =   tcost (isort (x:xs)) 
+  === tcost (bbind (length xs) (isort xs) (insert x))
+  === tcost (isort xs) + tcost ((insert x) (tval (isort xs)))
+      ? isortSortedVal xs 
+  === tcost (isort xs) + tcost (insert x xs)
+      ? theorem xs 
+  =<= length xs + 1
+  =<= length (x:xs)
+  *** QED 
+
+
+isortSortedVal :: Ord a => [a] -> () 
+{-@ isortSortedVal :: Ord a => xs:OList a -> { tval (isort xs) == xs }  @-}
+isortSortedVal [] = () 
+isortSortedVal [_] = () 
+isortSortedVal (x:xs) 
+  =   tval (isort (x:xs))
+  === tval (bbind (length xs) (isort xs) (insert x)) 
+  === tval ((insert x) (tval (isort xs)))
+       ? isortSortedVal xs 
+  === tval (insert x xs) 
+  === (x:xs)
+  *** QED 
